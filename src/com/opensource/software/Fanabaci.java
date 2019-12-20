283200b7020401d259ac421f3510fdc5fde7827a
kdaud <dkakumirizii@gmail.com>
2019-12-20 03:25:34 +0300

    Finobachi codes

---
+++ src/com/opensource/software/Fanabaci.java
@@ -0,0 +1,29 @@
+package com.opensource.software;
+
+import java.util.Scanner;
+
+public class Fanabaci {
+	
+	public static void main(String[] args) {
+		
+
+		System.out.println("Type in index of fibbonaci sequence:");
+		
+		int seq = 0;
+		Scanner sc = new Scanner(System.in);
+		seq = sc.nextInt();
+		
+		System.out.println(fib(seq));
+	}
+	
+	public static int fib(int n) {
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		else
+			return fib(n - 1) + fib(n - 2);
+		
+	}
+	
+}
