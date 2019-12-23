62cfb90e2405fdb19eb7ab2c899bd7f4a8b0c6d4
kdaud <dkakumirizii@gmail.com>
2019-12-23 14:30:09 +0300

    added files

---
+++ src/com/patients/codes/Main.java
@@ -0,0 +1,17 @@
+package com.patients.codes;
+
+import java.util.Date;
+
+public class Main {
+	
+	static {
+		System.out.println("I LOVE JAVA CODING");
+	}
+	
+	public static void main(String[] args) {
+		Maths obj = new Maths();
+		obj.setDate(new Date());
+		System.out.println(obj.getDate());
+	}
+	
+}
---
+++ src/com/patients/codes/Maths.java
@@ -0,0 +1,31 @@
+package com.patients.codes;
+
+import java.util.Date;
+
+public class Maths {
+	
+	private Date date;
+	
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+	
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Maths [date=" + date + "]";
+	}
+	
+}
