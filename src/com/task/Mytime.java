72c04c75daf7b480ff851e8fdb7401cf3e6d8376
kdaud <dkakumirizii@gmail.com>
2020-01-09 18:25:40 +0300

    files in code

---
+++ src/com/task/Mytime.java
@@ -0,0 +1,39 @@
+package com.task;
+
+import java.util.Date;
+
+public class Mytime {
+	
+	private Date startDate;
+	
+	private Date endDate;
+	
+	/**
+	 * @return Return the startDate
+	 */
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	/**
+	 * @param startDate The startDate to set
+	 */
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	/**
+	 * @return Return the endDate
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	/**
+	 * @param endDate The endDate to set
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+}
