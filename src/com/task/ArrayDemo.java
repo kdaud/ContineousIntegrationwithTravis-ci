368456d88a1c638b05d140542d90aaae07009742
kdaud <dkakumirizii@gmail.com>
2020-01-21 23:02:23 +0300

    files

---
+++ src/com/task/ArrayDemo.java
@@ -0,0 +1,14 @@
+package com.task;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+	
+	private Scanner scanner = new Scanner(System.in);
+	
+	private void gciover() {
+		System.out.println("Enter the number of arrays:");
+		
+	}
+	
+}
